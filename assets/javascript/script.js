79a0a0fa88012ce6902f75e6dcb0b953325193c4
Hatshepsut <amabelle.delacruz322@gmail.com>
2018-12-09 18:28:29 -0800

    fixing javascript

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -9,11 +9,81 @@ for (var i = 0; i < topics.length; i++) {
 };
 
 //on click event for each button
-// $("button").on("click", function () {
+$("button").on("click", function () {
 //     //getting attribute "data-person" and adding url
-//     var person = $(this).attr("data-person");
-//     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-//         person + "&api_key=dc6zaTOxFJmzC&limit=10";
+    var animal = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        animal + "&api_key=dc6zaTOxFJmzC&limit=10";
+        console.log(queryURL);
+
+// added code
+
+//this function contains api connection, ajax call and redering of returned data to div tag display
+// function gifDisplay() {
+//     var event = $(this).attr("data-name");
+//     //api url string
+//     var apiURL =
+//       "https://api.giphy.com/v1/gifs/search?q=" +
+//       event +
+//       "&api_key=sYAxNyVTIWOWfoyFGSvZwDdC3wwKOOTR&limit=12";
+//     console.log(apiURL);
+    $.ajax({
+      url: apiURL,
+      method: "GET"
+    }).done(function(response) {
+      console.log(response);
+      $("#gifsAppear").empty();
+      var results = response.data;
+      if (results == "") {
+        alert("There isn't a gif for this selected button");
+      }
+      //for loop generates the 12 renered images
+      for (var i = 0; i < results.length; i++) {
+        var gifDiv = $("<div>"); //div for the gifs to go inside
+        gifDiv.addClass("gifDiv");
+
+        // pulling gif
+        var gifImage = $("<img>");
+        gifImage.attr("src", results[i].images.fixed_height_small_still.url); // still image stored into src of image
+        gifImage.attr(
+          "data-still",
+          results[i].images.fixed_height_small_still.url
+        ); // still image
+        gifImage.attr("data-animate", results[i].images.downsized.url); // animated image
+
+        gifImage.addClass("image");
+        gifDiv.append(gifImage);
+        $("#myGifs").prepend(gifDiv);
+
+        // pulling rating of gif
+        var gifRating = $("<p class='rate-card'>").text(
+          "Rating: " + results[i].rating
+        );
+        gifDiv.append(gifRating);
+      }
+    });
+  }
+  //calling the functions
+  buttonsDisplay();
+  newButtonAdd();
+  removeButton();
+  // Document Event Listeners using global settings to work image click actions
+  $(document).on("click", ".event", gifDisplay);
+  $(document).on("click", ".image", function() {
+    var state = $(this).attr("data-state");
+    if (state == "still") {
+      $(this).attr("src", $(this).data("animate"));
+      $(this).attr("data-state", "animate");
+    } else {
+      $(this).attr("src", $(this).data("still"));
+      $(this).attr("data-state", "still");
+    }
+  });
+});
+
+
+// end of added code
+
 
 //     $.ajax({
 //         url: queryURL,
